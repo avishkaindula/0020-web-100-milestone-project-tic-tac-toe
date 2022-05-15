058a81a36c9cd74869f8a7ac1f37fbd006e6cf1a
Avishka Indula <avishkainduladev@gmail.com>
2022-05-15 22:20:09 +0530

    form submission with JS. new FormData()

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,7 @@ const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
 // even though app.js is linked after config.js, we can still write this constants in app.js.
 // It's possible because click event listener occurs "after" the above code lines are being executed.
+const formElement = document.querySelector("form");
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
@@ -17,4 +18,14 @@ editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
 cancelConfigBtnElement.addEventListener("click", closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
 // We can add click event listeners not only to buttons, but also to other elements.
-// So if we even click the backdrop, the overlay will disappear. 
+// So if we even click the backdrop, the overlay will disappear.
+
+formElement.addEventListener("submit", savePlayerConfig);
+// The default mechanism of a submit button is to send a http request to a server.
+// But we need read the value of the form and set the value as the player's name.
+// We do it by adding as event to the "form" like this.
+// We are adding an event to the "overall form", not to the "submit button".
+// But it's actually gives the same result of adding a click event to the submit button.
+// So we can either add a click event to the submit button, or add a "submit" event to the overall form.
+// Submit is the built in event just like a click event.
+// So the savePlayerConfig function will execute when the form is submitted.
--- scripts/config.js
+++ scripts/config.js
@@ -7,3 +7,31 @@ function closePlayerConfig() {
   playerConfigOverlayElement.style.display = "none";
   backdropElement.style.display = "none";
 }
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  // This is how we stop the default mechanism of a submit button of sending a http request to a server.
+  // So now if we click the submit button, the page won't get reloaded.
+
+  const formData = new FormData(event.target);
+  // This is actually a object created by us.
+  // this is same as creating a object like const formData = {}; this.
+  // This is another way of creating such a object by using the "new" keyboard.
+  // The we add a build in function called FormData(). This is actually known as a built in blueprint.
+  // What we doing here with new FormData() is called instantiating an object based on an object blueprint.
+  // FormData is a function that knows how to generate objects that have a certain shape.
+  // We can use such blueprints by executing these blueprint functions as functions and adding the "new" keyword in front.
+  // FormData is a built-in blueprint that actually takes a form, so a pointer at a form HTML element,
+  // and will then automatically extract values entered into inputs in that form for us.
+  // We can achieve this by using event.target inside the FormData() function,
+  // because event.target points at the HTML element (form element) that was responsible for the event.
+  // Now this will automatically look for inputs inside the form.
+
+  const enteredPlayerName = formData.get("player-name");
+  // player-name is the value we've defined on the name attribute of the input element.
+  // So that would be the identifier we need to get access to the entered input value.
+  // get is a special method that's available on the formData.
+  // And this is how we extract information from a form.
+
+  console.log(enteredPlayerName);
+}
